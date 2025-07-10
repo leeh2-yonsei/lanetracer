1700c3fbad927dfdd60afcbd686377c1a17674d4
leehy <leehyunho2006@gmail.com>
2025-07-10 16:42:22 +0900

    feat: add control feature at arduino.py
    Standard: 2.9

--- hardware/arduino.py
+++ hardware/arduino.py
@@ -2,6 +2,9 @@ import serial
 
 serial = serial.Serial('/dev/ttyACM0', 9600)
 
-def send(info: int):
+def send(info: int | str):
     serial.write(str(info).encode())
 
+if __name__ == "__main__":
+    messg = input("Input the message to send:")
+    send(messg)
