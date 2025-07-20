f517365e62005003eab8f3ef0e10471a7f09856d
leehy <leehyunho2006@gmail.com>
2025-07-20 16:55:58 +0900

    fix: main 처리시 이미지 좌우 반전
    Standard: 3.2

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ def main(root:str, mode:str):
     img = Image.open(root).convert('L')
     img = ImageOps.exif_transpose(img)
     img = img.resize((100, 75))
+    img = img.transpose(method=Image.Transpose.FLIP_TOP_BOTTOM)
     original_img = np.array(img) / 255.
     img = binaryzation(img, 0.5, correction=True)  # <y, x>
     img = classify(img, 1)
